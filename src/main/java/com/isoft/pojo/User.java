650fe798b02281cfb15aaff3b1ee2083e13de4d9
calculateWorld <darkforever@outlook.com>
2019-01-14 11:50:38 +0800

    init

--- src/main/java/com/isoft/pojo/User.java
+++ src/main/java/com/isoft/pojo/User.java
@@ -3,9 +3,8 @@ package com.isoft.pojo;
 public class User {
     private int id;
     private String uname;
-    private String upwd;
-    private String lastLoginTime;
-    private String role;
+    private String runame;
+    private String remark;
 
     public int getId() {
         return id;
@@ -13,14 +12,6 @@ public class User {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getLastLoginTime() {
-        return lastLoginTime;
-    }
-
-    public void setLastLoginTime(String lastLoginTime) {
-        this.lastLoginTime = lastLoginTime;
     }
 
     public String getUname() {
@@ -31,19 +22,24 @@ public class User {
         this.uname = uname;
     }
 
-    public String getUpwd() {
-        return upwd;
+    public String getRuname() {
+        return runame;
     }
 
-    public void setUpwd(String upwd) {
-        this.upwd = upwd;
+    public void setRuname(String runame) {
+        this.runame = runame;
     }
 
-    public String getRole() {
-        return role;
+    public String getRemark() {
+        return remark;
     }
 
-    public void setRole(String role) {
-        this.role = role;
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    @Override
+    public String toString() {
+        return "["+uname+","+runame+"]";
     }
 }
